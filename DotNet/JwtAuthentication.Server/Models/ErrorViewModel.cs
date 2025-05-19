ed26e442a334e0274662588b5108ea7948515e48
Oliver Forral <intrepion@gmail.com>
2025-05-19 14:32:38 -0600

    dotnet new mvc --name JwtAuthentication.Server

---
+++ DotNet/JwtAuthentication.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JwtAuthentication.Server.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
