38cd5cfdd71e4bc490de38ec3d57e94854051358
Oliver Forral <intrepion@gmail.com>
2025-05-19 16:19:46 -0600

    The Login Component for the Angular JWT Authentication

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginModel } from '../_interfaces/login.model';
+import { Router } from '@angular/router';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import { AuthenticatedResponse } from '../_interfaces/authenticated-response.model';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +11,30 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  invalidLogin: boolean = true;
+  credentials: LoginModel = {username:'', password:''};
 
+  constructor(private router: Router, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  login = ( form: NgForm) => {
+    if (form.valid) {
+      this.http.post<AuthenticatedResponse>("https://localhost:5001/api/auth/login", this.credentials, {
+        headers: new HttpHeaders({ "Content-Type": "application/json"})
+      })
+      .subscribe({
+        next: (response: AuthenticatedResponse) => {
+          const token = response.token;
+          localStorage.setItem("jwt", token); 
+          this.invalidLogin = false; 
+          this.router.navigate(["/"]);
+        },
+        error: (err: HttpErrorResponse) => this.invalidLogin = true
+      })
+    }
+  }
 }
